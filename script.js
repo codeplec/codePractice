46e5b46b96409b7a7818009ef4ec31b6bc042414
codeplec <nelsonzh109@gmail.com>
2015-07-31 11:48:03 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('div').fadeOut(1000);
+    // Target element 'div' will disapper (fadeout) upon document loading
+    // My guess is that (1000) represents 1 sec -> 1000 miliseconds 
+});
